17717efad0f7682b2588d0b92470b6fc7e598d69
zanlei <18817832003@qq.com>
2023-09-03 10:55:58 +0800

    feat: 初步测试

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,58 @@
-import {tensor,scalar, variable, train} from "@tensorflow/tfjs"
-import {Point2D, render} from "@tensorflow/tfjs-vis"
+import {
+  tensor,
+  scalar,
+  variable,
+  train,
+  sequential,
+  layers,
+  losses,
+  browser,
+} from "@tensorflow/tfjs";
+import { Point2D, render, show } from "@tensorflow/tfjs-vis";
+import "@tensorflow/tfjs-backend-webgl";
 
 // y = 2x + 3
-const dataX = tensor([1,2,3,4,5,6,7,8,9])
-const dataY = dataX.mul(scalar(2)).add(scalar(3))
+const dataX = tensor([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+const dataY = dataX.mul(scalar(2)).add(scalar(3));
 
-render.scatterplot({name:"线性回归训练集"},{values:[1,2,3,4,5,6,7,8,9].map((x)=>{
-    return {
+
+render.scatterplot(
+  { name: "线性回归训练集" },
+  {
+    values: [1, 2, 3, 4, 5, 6, 7, 8, 9].map((x) => {
+      return {
         x,
-        y:2*x+3
-    } as Point2D
-}),series:["默认数据"]})
-
-// 尝试拟合
-const k = variable(scalar(Math.random()))
-const b = variable(scalar(Math.random()))
+        y: 2 * x + 3,
+      } as Point2D;
+    }),
+    series: ["默认数据"],
+  }
+);
 
 // 优化器
-const opt = train.sgd(0.01)
+const opt = train.sgd(0.02);
+
+const model = sequential();
+model.add(
+  layers.dense({
+    units: 1,
+    inputShape: [1],
+  })
+);
+
+model.compile({
+  loss: losses.meanSquaredError,
+  optimizer: opt,
+});
 
 // 训练
-for (let index = 0; index < 10000; index++) {
-    opt.minimize(()=>{
-        const perdict = k.mul(dataX).add(b)
-        const loss = perdict.sub(dataY).square().mean()
-        // loss.print()
-        return loss.asScalar()
-    })
-}
+await model.fit(dataX, dataY, {
+  batchSize: 8,
+  epochs: 160,
+  callbacks: show.fitCallbacks({ name: "训练" }, ["loss"]),
+});
 
-
-
-// 训练结果
-console.log("result")
-k.print()
-b.print()
+console.log("weight");
+model.getWeights().forEach((e) => {
+  e.print();
+});
